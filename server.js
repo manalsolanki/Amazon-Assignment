8700efa7c5c4d9df6c169b76a06255d489ae0c14
Manal Solanki <60120657+manalsolanki@users.noreply.github.com>
2020-06-21 23:08:09 -0400

    Changed the port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const exphbs  = require('express-handlebars');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000
 
 const CategoryDB = require("./models/product-category");
 const ProductDB = require("./models/product-list");
